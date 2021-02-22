0ed90082f72a361bcb94d99c944997408c9c5959
Ana Bajo <kozicana@yahoo.com>
2021-02-22 19:09:07 +0100

    edits to limitations

--- app/controllers/limitations_controller.rb
+++ app/controllers/limitations_controller.rb
@@ -7,13 +7,12 @@ class LimitationsController < ApplicationController
 
   def create
     @bike = Bike.find(params[:bike_id])
-
+    @limitation = Limitation.new(limitations_params)
   end
 
   private
 
   def limitations_params
-    params.require(:limitation).permit(
-      )
+    params.require(:limitation).permit(:customization_category_id, :customization_option_id, :allowed_options_ids)
   end
 end
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,6 +3,7 @@ class Bike < ApplicationRecord
   # has_many :customization_options, through: :customization_categories
   has_many :orders
 
+
   validates :name, presence: true
   accepts_nested_attributes_for :customization_categories, reject_if: lambda { |attributes| attributes['bike_element'].blank? }
 
